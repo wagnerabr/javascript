80020992ef326bcc6c5098d47cab14d7679d68dd
Wagner <wagner.abr@hotmail.com>
2020-07-13 14:21:16 -0300

    Example booleanos

---
+++ cap01/booleanos.js
@@ -0,0 +1,17 @@
+const podeDIrigir = false
+
+if(podeDIrigir) console.log("Pode dirigir")
+
+const saldoConta = 1
+
+if(!saldoConta) console.log("Não tem saldo")
+
+const boolComString = "Hello"
+
+//Forca o valor true ou false
+//de acordo com a tabela
+// muito usado na empresas para mostrar o tipo booleano da variavel
+console.log('boolComString', !!boolComString);
+
+//negacao 
+console.log('negação', !boolComString);
